0e78df3effba3ae72228ec120ac0b7d48e972661
ruben <blenicio777650@gmail.com>
2016-08-18 15:17:49 -0300

    Commit inicial para criação do projeto.

---
+++ CreativeAPI/src/main/java/com/ufc/CreativeApiApplication.java
@@ -0,0 +1,12 @@
+package com.ufc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativeApiApplication.class, args);
+	}
+}
